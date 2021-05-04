15e85effa13144822c52396e936e0bf95c1dbba7
nikolayonok <nikolayonokk@yandex.ru>
2021-05-04 20:23:19 +0300

    2st load

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Lesson1 {
     public static void main(String[] args) {
-       int a = 2;
-        }
+        System.out.println("введите первое число");
+        int a = new Scanner(System.in).nextInt();
+        System.out.println("введите второе число");
+        int b = new Scanner(System.in).nextInt();
+        System.out.println("введите третье число");
+        int c = new Scanner(System.in).nextInt();
+
+
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+
     }
+}
 
 
