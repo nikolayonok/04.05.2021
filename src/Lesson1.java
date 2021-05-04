3879eb03371256817c3f8c5c87fc220b91abbaec
nikolayonok <nikolayonokk@yandex.ru>
2021-05-04 19:20:30 +0300

    2st load

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -1,17 +1,7 @@
 public class Lesson1 {
     public static void main(String[] args) {
-        int i = 0;
-        while (i < 20) {
-            if (i < 10) {
-                System.out.println("Я меньше 10 " + i);
-            } else {
-                System.out.println("Я больше 10 " + i);
-            }
-            if (i == 10)
-                System.out.println("Я равен 10");
-            i++;
+       int a = 2;
         }
     }
 
 
-}
