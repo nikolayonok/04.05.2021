7a31eb4f2c579c9c380a5e1383c4ddfc554923d8
nikolayonok <nikolayonokk@yandex.ru>
2021-05-06 18:12:14 +0300

    task4

---
+++ src/task4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите данные ");
+        double a = new Scanner(System.in).nextInt();
+        double h = new Scanner(System.in).nextInt();
+        System.out.println((a * h) / 2);
+
+
+    }
+}
