32d54c142a17f5315b3c9c4b60858fe4d316ea8a
nikolayonok <nikolayonokk@yandex.ru>
2021-05-06 19:57:37 +0300

    task5

--- src/task5.java
+++ src/task5.java
@@ -4,14 +4,9 @@ public class task5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число ");
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        int d = scanner.nextInt();
-        System.out.print(d);
-        System.out.print(c);
-        System.out.print(b);
-        System.out.print(a);
-
+        String value = scanner.nextLine();
+        for(int i = value.length() - 1; i >= 0; i--) {
+            System.out.print(value.charAt(i));
+        }
     }
 }
